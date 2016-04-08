5d807df99394cbfe2312ff240d470650506ef1d2
Roi Docampo <roidocampo@com.fake>
2016-04-08 13:06:53 +0200

    added statcounter

---
+++ statcounter.js
@@ -0,0 +1,37 @@
+var sc_project=10906088; 
+
+var sc_invisible=1; 
+
+var sc_security="ea06625d"; 
+
+var _sc_js_host = (("https:" == document.location.protocol) ?
+                   "https://secure." : "http://www.");
+
+document.write(
+        "<sc" +
+        "ript type='text/javascript' src='" +
+        _sc_js_host +
+        "statcounter.com/counter/counter.js'></" + 
+        "script>"
+        );
+
+// Original Code
+// =============
+//
+// <!-- Start of StatCounter Code for Default Guide -->
+// <script type="text/javascript">
+// var sc_project=10906088; 
+// var sc_invisible=1; 
+// var sc_security="ea06625d"; 
+// var scJsHost = (("https:" == document.location.protocol) ?
+// "https://secure." : "http://www.");
+// document.write("<sc"+"ript type='text/javascript' src='" +
+// scJsHost+
+// "statcounter.com/counter/counter.js'></"+"script>");
+// </script>
+// <noscript><div class="statcounter"><a title="web counter"
+// href="http://statcounter.com/" target="_blank"><img
+// class="statcounter"
+// src="http://c.statcounter.com/10906088/0/ea06625d/1/"
+// alt="web counter"></a></div></noscript>
+// <!-- End of StatCounter Code for Default Guide -->
